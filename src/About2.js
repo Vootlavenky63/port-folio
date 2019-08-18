e3982f6d736165c04384e2735019edcfff29f79f
vootlavenky63 <vootla.venky63@gmail.com>
2019-08-18 11:25:52 +0530

    react Router home/user/about

--- src/About2.js
+++ src/About2.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default function About2() {
     return (
         <div>
-            <h2>f you’re interested in playing around with React, you can use an online code playground. Try a Hello World template on CodePen, CodeSandbox, or Glitc</h2>
         </div>
     )
 }
